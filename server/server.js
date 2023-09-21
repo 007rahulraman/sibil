59e2ff1225597e51c74b574fdbcd3cba8a7d3a8b
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2023-09-22 03:37:27 +0530

    Adding comments

--- server/server.js
+++ server/server.js
@@ -9,18 +9,20 @@ const app = express();
 
 app.use("^/$", (req, res) => {
   fs.readFile(path.resolve("./build/index.html"), "utf8", (err, data) => {
+    //reading file from the build folder and convert it.
     if (err) {
       console.log(err);
       return res.status(500).send("Some error occured");
     }
     const html = ReactDOMServer.renderToString(
+      //we need to provide our app component to convert React component to HTML format
       <StaticRouter location={req.url}>
         <App />
       </StaticRouter>
     );
 
     return res.send(
-      data.replace('<div id="root"></div>', `<div id="root">${html}</div>`)
+      data.replace('<div id="root"></div>', `<div id="root">${html}</div>`) //now we need to replace the build index.html with the app component which we converted from the react component.
     );
   });
 });
